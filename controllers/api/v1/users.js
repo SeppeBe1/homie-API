789ddb1fe923cae54aadb4716bcda3a2e0ceb48f
unknown <seppe.bellens@hotmail.nl>
2023-03-09 12:00:56 +0100

    Build: full user model

--- controllers/api/v1/users.js
+++ controllers/api/v1/users.js
@@ -16,10 +16,31 @@ function getUser(req, res) {
 
 function createUser(req, res) {
   const firstName = "Seppe";
-  const lastName = "Bellens"
+  const lastName = "Bellens";
+  const email = "Seppe@gmail.com";
+  const password = "test";
+  const house_Id = "1";
+  const houseAdmin = false;
+  const profilePic = "";
+  const serverAdmin = false;
+  const notification = false;
+  const nightmode = false;
+  const language = "Dutch";
+  const shareData = false;
+  
   const newUser = new User({
     firstName,
     lastName,
+    email,
+    password,
+    house_Id,
+    houseAdmin,
+    profilePic,
+    serverAdmin,
+    notification,
+    nightmode,
+    language,
+    shareData,
   });
 
   newUser.save()
--- models/User.js
+++ models/User.js
@@ -4,6 +4,16 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     firstName: String,
     lastName: String,
+    email: String,
+    password: String,
+    house_Id:  Number,
+    houseAdmin: Boolean,
+    profilePic: String,
+    serverAdmin: Boolean,
+    notification: Boolean,
+    nightmode: Boolean,
+    language: String,
+    shareData: Boolean,
 });
 
 const User = mongoose.model("User", userSchema);

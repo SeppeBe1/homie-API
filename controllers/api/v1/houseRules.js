97cd30688e6b9e397d8a816764035b50bcef6bd0
Jade Apers <jadeapers@telenet.be>
2023-06-15 20:37:56 +0200

    fix: edit api houserules

--- controllers/api/v1/houseRules.js
+++ controllers/api/v1/houseRules.js
@@ -1,96 +1,108 @@
 const Houserule = require("../../../models/HouseRules")
 
 function getHouseRule(req, res) {
-  
-    Houserule.find({ })
-      .then(houserule => {
-        if (!houserule) {
-          res.status(404).json({ error: 'houserule not found' });
-        } else {
-          res.json(houserule);
-        }
-      })
-      .catch(err => res.status(500).json(err));
-  }
+  const { houseId } = req.params;
+  Houserule.find({ houseId: houseId })
+  .then(results => {
+    if (results.length === 0) {
+       res.status(404).json({ "status": "Houserules not found" });
+    }else {
+      res.json({
+        "status": "success",
+        "result": results.map(result => ({
+          "type": result.type,
+          "description": result.description,
+          "houseId": result.houseId,
+        }))
+      });
+    }
+})
+.catch(err => res.status(500).json({
+  "status": "error",
+  "message": err
+}));
+}
+
   
 
-  const createHouseRule = async (req, res, next) => {
-    const description = req.body.description;
-    const houseId = req.body.houseId;
-  
-    const houserule = new Houserule({
-      description: description,
-      houseId: houseId,
+const createHouseRule = async (req, res, next) => {
+  const description = req.body.description;
+  const houseId = req.body.houseId;
 
+  const houserule = new Houserule({
+    "description": description,
+    "houseId": houseId,
+  });
+
+  try {
+    const result = await houserule.save();
+    console.log(result);
+
+    res.json({
+      "status": "success",
+      "result": result,
+      "data": {
+        "description": result.description,
+        "houseId": result.houseId,
+      }
     });
-  
-    await houserule.save().then(result => {
-        console.log(result);
-  
-        res.json({
-            "status":"succes",
-            "result": result,
-            "data": {
-                "description": result.description,
-                "houseId": result.houseId,
-            }
-        });
-    }).catch(error => {
-        res.json({
-            "status":"error",
-            "message":error,
-        });
-    }); 
-  };
-
-
-  function updateHouseRule(req, res) {
-    const { description, houseId } = req.params;
-    const newDescription = req.body.description;
-  
-    Houserule.findOneAndUpdate({ description: description, houseId: houseId },{ description: newDescription})
-      .then(result => {
-        if (!result) {
-          res.status(404).json({ 
-            "status":"failed",
-            "status":"does not exist",
-
-          });
-        } else {
-          res.json({
-            "status":"succes",
-            "status":result,
-          });
-        }
-      })
-      .catch(error => res.status(500).json({ 
-        "status":"failed",
-        "result": error,
-      }));
+  } catch (error) {
+    res.status(500).json({
+      "status": "error",
+      "message": error,
+    });
   }
+};
 
-  function deleteHouseRule(req, res) {
-    const { description, houseId } = req.params;
-  
-    Houserule.findOneAndDelete({ description: description, houseId: houseId })
-      .then(deleteHouseRule => {
-        if (!deleteHouseRule) {
-          res.status(404).json({ 
-            "status":"failed",
-            "status":"does not exist",
 
-          });
-        } else {
-          res.json({
-            "status":"succes",
-          });
-        }
-      })
-      .catch(error => res.status(500).json({ 
-        "status":"failed",
-        "result": error,
-      }));
-  }
+
+function updateHouseRule(req, res) {
+  const { description, houseId } = req.params;
+  const newDescription = req.body.description;
+
+  Houserule.findOneAndUpdate({ description: description, houseId: houseId },{ description: newDescription})
+    .then(result => {
+      if (!result) {
+        res.status(404).json({ 
+          "status":"failed",
+          "status":"does not exist",
+
+        });
+      } else {
+        res.json({
+          "status":"succes",
+          "status":result,
+        });
+      }
+    })
+    .catch(error => res.status(500).json({ 
+      "status":"failed",
+      "result": error,
+    }));
+}
+
+function deleteHouseRule(req, res) {
+  const { description, houseId } = req.params;
+
+  Houserule.findOneAndDelete({ description: description, houseId: houseId })
+    .then(deleteHouseRule => {
+      if (!deleteHouseRule) {
+        res.status(404).json({ 
+          "status":"failed",
+          "status":"does not exist",
+
+        });
+      } else {
+        res.json({
+          "status":"succes",
+        });
+      }
+    })
+    .catch(error => res.status(500).json({ 
+      "status":"failed",
+      "result": error,
+    }));
+}
 
 
 module.exports.getHouseRule = getHouseRule;
--- routes/api/v1/houseRules.js
+++ routes/api/v1/houseRules.js
@@ -3,20 +3,15 @@ const router = express.Router();
 const houseruleController = require("../../../controllers/api/v1/houseRules");
 
 // GET /api/vi/users
-
-router.get("/",houseruleController.getHouseRule);
-
+router.get("/:houseId",houseruleController.getHouseRule);
 
 // POST /api/vi/users
-
 router.post("/", houseruleController.createHouseRule);
 
 // PUT /api/vi/users
-
 router.put("/:description/:houseId", houseruleController.updateHouseRule);
 
 // DELETE /api/vi/users
-
 router.delete("/:description/:houseId", houseruleController.deleteHouseRule);
 
 module.exports = router;

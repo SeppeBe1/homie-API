f806e05d322db0d992c314d9d5644068377adba2
Jade Apers <jadeapers@telenet.be>
2023-06-16 02:52:49 +0200

    merged

--- controllers/api/v1/anouncement.js
+++ controllers/api/v1/anouncement.js
@@ -1,39 +1,39 @@
 const Anouncement = require("../../../models/Anouncement");
-const moment = require('moment');
-require('moment-timezone');
+const moment = require("moment");
+require("moment-timezone");
 
 function getAnnouncement(req, res) {
   const { houseId } = req.params;
   Anouncement.find({ houseId: houseId })
-    .then(results => {
+    .then((results) => {
       if (results.length === 0) {
-        res.status(404).json({ 
-          "status": "failed"
+        res.status(404).json({
+          status: "failed",
         });
       } else {
         res.json({
-          "status": "success",
-          "result": results.map(result => ({
-            "_id": result._id,
-            "type": result.type,
-            "description": result.description,
-            "houseId": result.houseId,
+          status: "success",
+          result: results.map((result) => ({
+            _id: result._id,
+            type: result.type,
+            description: result.description,
+            houseId: result.houseId,
             // Add other properties you want to include
-          }))
+          })),
         });
       }
     })
-    .catch(err => res.status(500).json({
-      "status": "error",
-      "message": err
-    }));
+    .catch((err) =>
+      res.status(500).json({
+        status: "error",
+        message: err,
+      })
+    );
 }
 
-
 function createAnouncement(req, res) {
-
   const type = req.body.type;
-  const activity = req.body.type;
+  const activity = req.body.activity;
   const description = req.body.description;
   const creatorId = req.body.creatorId;
   const houseId = req.body.houseId;
@@ -50,80 +50,85 @@ function createAnouncement(req, res) {
   const datePlanned = req.body.datePlanned;
   const dateCreated = req.body.dateCreated;
   const beHomieNotificationTime = req.body.beHomieNotificationTime;
-  
+
   const newAnouncement = new Anouncement({
-    "type": type ,
-    "activity": activity,
-    "description": description,
-    "creatorId": creatorId,
-    "houseId": houseId,
-    "participants": participants,
+    type: type,
+    activity: activity,
+    description: description,
+    creatorId: creatorId,
+    houseId: houseId,
+    participants: participants,
+    
 
-    "item": item,
-   "forWho_id": forWho_id,
-    "amount": amount,
-    "percentage": percentage,
-    "paid": paid,
-    "receiptImage": receiptImage,
+    item: item,
+    forWho_id: forWho_id,
+    amount: amount,
+    percentage: percentage,
+    paid: paid,
+    receiptImage: receiptImage,
 
-    "behomieTime": behomieTime,
-    "datePlanned": datePlanned,
-    "dateCreated": dateCreated,
-    "beHomieNotificationTime": beHomieNotificationTime,
+    behomieTime: behomieTime,
+    datePlanned: datePlanned,
+    dateCreated: dateCreated,
+    beHomieNotificationTime: beHomieNotificationTime,
   });
 
-   newAnouncement.save().then(result => {
-
-    res.json({
-        "status":"succes",
-        "result": result,
-        "data": {
-          "type": result.type,
-          "description": result.description,
-          "activity": result.activity,
-          "participants": result.participants,
-        }
+  newAnouncement
+    .save()
+    .then((result) => {
+      res.json({
+        status: "succes",
+        result: result,
+        data: {
+          type: result.type,
+          description: result.description,
+          activity: result.activity,
+          participants: result.participants,
+          dateCreated: result.dateCreated,
+        },
+      });
+    })
+    .catch((error) => {
+      res.json({
+        status: "error",
+        message: error,
+      });
     });
-}).catch(error => {
-    res.json({
-        "status":"error",
-        "message":error,
-    });
-}); 
 }
 
-
 function updateAnouncement(req, res) {
-    const  {id}  = req.params;
-  
-    Anouncement.findByIdAndUpdate(id,  {type: "NormalAnouncement"}  , { new: true })
-      .then(result => {
-        if (!result) {
-          res.status(404).json({ error: 'Anouncement not found' });
-        } else {
-          res.json(result);
-        }
-      })
-      .catch(err => res.status(500).json(err));
-  }
+  const { id } = req.params;
 
-  function deleteAnouncement(req, res) {
-    const { id } = req.params;
-  
-    Anouncement.findByIdAndDelete(id)
-      .then(deleteAnouncement => {
-        if (!deleteAnouncement) {
-          res.status(404).json({ error: 'Anouncement not found' });
-        } else {
-          res.json(deleteAnouncement);
-        }
-      })
-      .catch(err => res.status(500).json(err));
-  }
+  Anouncement.findByIdAndUpdate(
+    id,
+    { type: "NormalAnouncement" },
+    { new: true }
+  )
+    .then((result) => {
+      if (!result) {
+        res.status(404).json({ error: "Anouncement not found" });
+      } else {
+        res.json(result);
+      }
+    })
+    .catch((err) => res.status(500).json(err));
+}
 
+function deleteAnouncement(req, res) {
+  const { id } = req.params;
+
+  Anouncement.findByIdAndDelete(id)
+    .then((deleteAnouncement) => {
+      if (!deleteAnouncement) {
+        res.status(404).json({ error: "Anouncement not found" });
+      } else {
+        res.json(deleteAnouncement);
+      }
+    })
+    .catch((err) => res.status(500).json(err));
+}
 
 module.exports.getAnnouncement = getAnnouncement;
 module.exports.createAnouncement = createAnouncement;
 module.exports.updateAnouncement = updateAnouncement;
 module.exports.deleteAnouncement = deleteAnouncement;
-
--- models/Anouncement.js
+++ models/Anouncement.js
@@ -2,22 +2,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const AnouncementSchema = new Schema({
-    type: String,
-    description: String,
-    creatorId: String,
-    houseId: String,
+  type: String,
+  description: String,
+  creatorId: String,
+  houseId: String,
+  activity: String,
 
-    item: String,
-    forWho_id:Number,
-    amount: Number,
-    percentage: Number,
-    paid: Boolean,
-    receiptImage: String,
+  item: String,
+  forWho_id: Number,
+  amount: Number,
+  percentage: Number,
+  paid: Boolean,
+  receiptImage: String,
 
-    behomieTime: Date,
-    datePlanned: Date,
-    dateCreated: String,
-    beHomieNotificationTime: Date,
+  behomieTime: Date,
+  datePlanned: Date,
+  dateCreated: String,
+  beHomieNotificationTime: Date,
 });
 
 const Anouncement = mongoose.model("Anouncement", AnouncementSchema);
